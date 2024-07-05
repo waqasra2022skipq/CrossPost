c50c40698e33355c28c65fe897a1d950ab2caa6e
waqasra2022skipq <waqas.rahman.sculptor@gmail.com>
2024-07-05 14:20:13 +0500

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/auth/redirect', function() {
+ return Socialite::driver('facebook')->redirect();
+});
+
+Route::get('/auth/callback', function() {
+    $user = Socialite::driver('facebook')->user();
+    Auth::login($user);
+ 
+    return redirect('/dashboard');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
